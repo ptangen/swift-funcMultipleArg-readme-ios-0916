2302a2d49a7d0a70394a0aea9234c1f7d6bdbdd7
Paul Tangen <ptangen@ptangen.com>
2016-09-06 11:21:22 -0400

    Complete the functions and multiple arguments assignment.

--- Moons.playground/Contents.swift
+++ Moons.playground/Contents.swift
@@ -7,7 +7,7 @@ moonsOfJupiter()
 
 // planetDescription, take 1
 // The bit of code below is commented out, since we iterate on this definition of planetDescription() below.
-/*
+
 func planetDescription(planet: String) {
     print("There are 67 moons orbiting \(planet).")
 }
@@ -17,7 +17,6 @@ planetDescription("Jupiter")
 var planet = "Mars"
 
 planetDescription(planet)
-*/
 
 
 func greet(name: String, greeting: String) {
@@ -35,9 +34,17 @@ func planetDescription(planet: String, numberOfMoons: Int) {
 
 planetDescription("Jupiter", numberOfMoons: 67)
 
-var planet = "Jupiter"
+planet = "Jupiter"
 var moons = 67
 planetDescription(planet, numberOfMoons: moons)
 planet = "Mars"
 moons = 2
 planetDescription(planet, numberOfMoons: moons)
+
+planet = "Mercury"
+moons = 89
+planetDescription(planet, numberOfMoons: moons)
+
+
+
+greet("Bill", greeting:"Welcome")
